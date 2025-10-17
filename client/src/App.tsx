2d17bd96e1ae28476509fd5c5b4adf0f8138cab1
RoHobie <ronitthakur267@gmail.com>
2025-10-17 15:28:09 +0530

    initial client setup with tailwind and Shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+function App() {
+
+  return (
+    <>
+      <div className="justify-center items-center flex gap-4 h-screen">
+      <Button>Do not click me</Button>
+      <Button>Click Me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
